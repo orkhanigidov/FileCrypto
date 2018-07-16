48021ec6f68d134f318de2a3fdb90040431d565d
iorxan <iorxan1995@gmail.com>
2018-07-16 13:19:12 +0400

    Added Cancel and Encrypt buttons. Added Icon, Name, Size labels.

--- src/com/filecrypto/gui/EncryptPage.java
+++ src/com/filecrypto/gui/EncryptPage.java
@@ -1,4 +1,37 @@
 package com.filecrypto.gui;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class EncryptPage {
+    private JPanel encryptPageView;
+    private JButton btnCancel;
+    private JButton btnEncrypt;
+    private JLabel lblIcon;
+    private JLabel lblName;
+    private JLabel lblSize;
+
+    public EncryptPage() {
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnEncrypt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private static void cancel() {
+
+    }
+
+    private static void encrypt() {
+
+    }
 }
