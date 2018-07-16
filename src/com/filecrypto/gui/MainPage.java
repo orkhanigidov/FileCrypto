0d64527683e53103b26a1f5d20c15384ea6a0958
iorxan <iorxan1995@gmail.com>
2018-07-16 12:45:29 +0400

    Create MainPage and added Upload File Button

--- src/com/filecrypto/gui/MainPage.java
+++ src/com/filecrypto/gui/MainPage.java
@@ -1,4 +1,16 @@
 package com.filecrypto.gui;
 
+import javax.swing.*;
+
 public class MainPage {
+    private JButton btnUploadFile;
+    private JPanel mainPageView;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("FileCrypto");
+        frame.setContentPane(new MainPage().mainPageView);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
