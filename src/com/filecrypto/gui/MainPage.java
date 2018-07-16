787bcfecaa602ac7e0d2f1839568a430d38db63c
iorxan <iorxan1995@gmail.com>
2018-07-16 13:55:19 +0400

    Changed Main Page frame size and added openFileChooser method

--- src/com/filecrypto/gui/MainPage.java
+++ src/com/filecrypto/gui/MainPage.java
@@ -1,15 +1,37 @@
 package com.filecrypto.gui;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainPage {
     private JButton btnUploadFile;
     private JPanel mainPageView;
 
+    public MainPage() {
+        btnUploadFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openFileChooser();
+            }
+        });
+    }
+
+    private static void openFileChooser() {
+        JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+
+        }
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("FileCrypto");
         frame.setContentPane(new MainPage().mainPageView);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(300, 300));
         frame.pack();
         frame.setVisible(true);
     }
