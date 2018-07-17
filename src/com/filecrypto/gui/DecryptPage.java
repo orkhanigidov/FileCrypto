c1cfd86d70a2a8c295368c10bf10a4c64acf1f2b
iorxan <iorxan1995@gmail.com>
2018-07-17 15:37:22 +0400

    Update DecryptPage GUI

--- src/com/filecrypto/gui/DecryptPage.java
+++ src/com/filecrypto/gui/DecryptPage.java
@@ -1,4 +1,26 @@
 package com.filecrypto.gui;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class DecryptPage {
+    private JPanel decryptPageView;
+    private JLabel lblIcon;
+    private JLabel lblName;
+    private JLabel lblSize;
+    private JButton btnCancel;
+    private JButton btnDecrypt;
+    private JPasswordField txtPassword;
+    private JTextField txtHint;
+    private static JFrame frame;
+    private static JFileChooser fileChooser;
+
+    public void showDecryptPage() {
+        frame = new JFrame("Decryption");
+        frame.setContentPane(new DecryptPage().decryptPageView);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(300, 300));
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
