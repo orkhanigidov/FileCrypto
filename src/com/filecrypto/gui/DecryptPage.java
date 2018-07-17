8cdd3a4491419a4b2f3451b9ecd5391bce63b970
iorxan <iorxan1995@gmail.com>
2018-07-17 15:39:58 +0400

    Created cancel method in DecryptPage

--- src/com/filecrypto/gui/DecryptPage.java
+++ src/com/filecrypto/gui/DecryptPage.java
@@ -2,6 +2,8 @@ package com.filecrypto.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DecryptPage {
     private JPanel decryptPageView;
@@ -14,6 +16,26 @@ public class DecryptPage {
     private JTextField txtHint;
     private static JFrame frame;
     private static JFileChooser fileChooser;
+
+    public DecryptPage() {
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cancel();
+            }
+        });
+        btnDecrypt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private static void cancel() {
+        frame.dispose();
+        new MainPage().showMainPage();
+    }
 
     public void showDecryptPage() {
         frame = new JFrame("Decryption");
